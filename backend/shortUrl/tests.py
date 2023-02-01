e9e0d4a609f07976dfb5f6e29d7679afd62c6b92
Sherwin Wyco <sherwin.wyco@aplaceformom.com>
2023-02-01 00:27:02 -0500

    added tests

--- backend/shortUrl/tests.py
+++ backend/shortUrl/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from .models import ShortUrl
+from .utils import generate_code
 
-# Create your tests here.
+
+class AnimalTestCase(TestCase):
+    def setUp(self):
+        ShortUrl.objects.create(
+            original_url="https://docs.djangoproject.com/en/4.1/topics/testing/overview/")
+        ShortUrl.objects.create(
+            original_url="https://docs.djangoproject.com/en/4.1/")
+
+    def test_short_urls_are_created_with_short_codes(self):
+        first = ShortUrl.objects.get(
+            original_url="https://docs.djangoproject.com/en/4.1/topics/testing/overview/")
+        second = ShortUrl.objects.get(
+            original_url="https://docs.djangoproject.com/en/4.1/")
+
+        self.assertTrue(first.short_code)
+        self.assertTrue(second.short_code)
+
+    def test_code_generator_length_is_7(self):
+        code = generate_code()
+        self.assertEqual(len(code), 7)
